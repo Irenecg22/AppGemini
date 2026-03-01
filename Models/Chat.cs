aa5eddbabea2eb889a827e01d551f14f4edddd1e
Irenecg22 <ireneca032006@gmail.com>
2026-03-01 12:19:58 +0100

    F

--- Models/Chat.cs
+++ Models/Chat.cs
@@ -6,7 +6,7 @@ namespace AppGemini.Models
 {
     public class Chat
     {
-        public string Role { get; set; } = "user"; // "user" o "model"
+        public string Role { get; set; } = "user"; 
         public string Text { get; set; } = "";
     }
 }
